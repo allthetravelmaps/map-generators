c868aa841a2369bcf589ff1e9dcaa6bd501b9172
Mike Fogel <mike@fogel.ca>
2017-10-04 19:30:01 -0300

    Remove now unused helpers and packaging

--- includes/manage-build-dir.bash
+++
@@ -1,8 +0,0 @@
-clean () {
-  echo "Cleaning out build dir $buildDir"
-  # avoid dangerously broad deletes (ie rm -rf)
-  find "$buildDir" -type f -d 1 -delete
-}
-
-mkdir -p "$buildDir"
-[ "$clean" = true ] && clean
